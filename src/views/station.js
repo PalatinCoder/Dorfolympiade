bb0b8fa866eb7c424fb35b1df2a0f0d4043d574b
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-12-09 17:33:43 +0100

    refactor(station-view): use data-table component

--- src/views/station.js
+++ src/views/station.js
@@ -1,8 +1,16 @@
 import { html } from '@polymer/lit-element';
 import { PageViewElement } from './page-view-element.js';
+import { DataTable } from '../components/data-table';
 
 class StationView extends PageViewElement {
     render() {
+        const data = [
+                ['Sunny', 'Schützen', 10],
+                ['Jessy', 'Schützen', 10],
+                ['Markus', 'Hütte', 10],
+                ['b9396386-6463-4249-bb9b-601a17a9d921', 'unbekannt', 10],
+            ];
+        const headings = ['Spieler', 'Gruppe', 'Punktzahl'];
         return html`
         <style>
             h2 { 
@@ -18,55 +26,13 @@ class StationView extends PageViewElement {
                 font-weight: normal;
                 color: #000000de; 
             }
-            table { width: 100%; }
-            table th, table td { 
-                height: 24px; /* 48 - padding */
-                padding: 12px;
-            }
-            table th {
-                border-bottom: 1px solid rgba(0,0,0,0.12);
-                text-align: left;
-                font-weight: normal;
-                font-size: 12px;
-                color: #00000099;
-            }
-            table td {
-                font-size: 14px;
-                color: #000000de;
-            }
-            table td:last-child { text-align: right; }
         </style>
         <h2>Feuerwehr</h2>
         <h3>Gespeicherte Ergebnisse</h3>
-        <table>
-            <thead>
-                <th>Spieler</th>
-                <th>Gruppe</th>
-                <th>Punktzahl</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Sunny</td>
-                    <td>Schützen</td>
-                    <td>10</td>
-                </tr>
-                <tr>
-                    <td>Jessy</td>
-                    <td>Schützen</td>
-                    <td>10</td>
-                </tr>
-                <tr>
-                    <td>Markus</td>
-                    <td>Hütte</td>
-                    <td>10</td>
-                </tr>
-                <tr>
-                    <td>b9396386-6463-4249-bb9b-601a17a9d921</td>
-                    <td>unbekannt</td>
-                    <td>10</td>
-                </tr>
-            </tbody>
-        </table>
+        <data-table 
+            .headings=${headings}
+            .data=${data}>
+        </data-table>
         `;
     }
 }
